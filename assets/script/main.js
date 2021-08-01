09da7dcb1b1130c111f0aa4c784f2ea6ec032f61
James Satherley <jamesrsatherley@gmail.com>
2021-08-01 14:10:32 +1200

    added js class and main

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,25 @@
+class conversion {
+    constructor(input, input_type, output_type) {
+        this.binary;
+        this.hex;
+        this.decimal;
+        this.input = input;
+        this.input_type = input_type;
+        this.output_type = output_type
+    }
+
+    convert(){
+        console.log(type, input_type, output_type)
+    }
+}
+
+
+
 function main() {
-    
+    var input = document.getElementById('input');
+    var input_type = document.getElementById('input_type');
+    var output_type = document.getElementById('output_type');
+
+    variable = new conversion();
+    variable.convert();
 }
